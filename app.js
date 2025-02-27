00fc488eda6297e072a70ea284e6571eea61ec0f
Eduardo Hernández <jhernandezg1403@gmail.com>
2025-02-27 16:38:30 -0600

    Añade sorteo de amigo secreto y boton de siguiente

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+
+//Listas
 let nombres = [];
-let botonAñadir = document.querySelector('button')
+let numerosSorteados=[]
+
+
 let nombreAmigo = document.getElementById('amigo')
 let listaAmigos = document.getElementById('listaAmigos')
+let amigoSorteado = document.getElementById('resultado')
+
+//Botones
+let botonSorteo = document.getElementById('btn-sorteo')
+let botonAñadir = document.querySelector('button')
+let botonSiguiente = document.getElementById('btn-siguiente')
+
 let valor = nombreAmigo.value
-console.log(valor)
+let amigoSecreto = 0
+
 
 botonAñadir.addEventListener('click', agregarAmigo)
 
@@ -12,10 +24,8 @@ function agregarAmigo() {
     if (valor=="") {
         alert('Ingresa un nombre válido')
     } else {
-        console.log(valor)
         nombres.push(valor)
         nombreAmigo.value = "";
-        console.log(nombres)
         mostrarAmigos()
     }
 }
@@ -29,3 +39,47 @@ function mostrarAmigos(){
     }
 }
 
+function sortearAmigo(){
+    amigoSorteado.innerText = ""
+    
+
+    if (nombres.length < 2){
+        alert("Debes ingresar al menos dos amigos para sorterar")
+    } else {
+        amigoSecreto = Math.floor(Math.random() * nombres.length)
+        while (numerosSorteados.includes(amigoSecreto)) {
+            amigoSecreto = Math.floor(Math.random() * nombres.length)
+            if (numerosSorteados.length == nombres.length){
+                break;
+            }
+        }
+        numerosSorteados.push(amigoSecreto)
+        console.log(numerosSorteados)
+        console.log(amigoSecreto)
+        let agregaResultado = document.createElement('p')
+        if (numerosSorteados.length==nombres.length+1){
+            agregaResultado.textContent = `Ya se han sorteado todos los amigos, éxito`
+            botonSorteo.disabled = true
+            botonAñadir.disabled = true
+            botonSiguiente.disabled = true
+        } else{
+            agregaResultado.textContent = `Tu amigo secreto es ${nombres[amigoSecreto]}`
+            botonSorteo.disabled = true
+        }
+        amigoSorteado.appendChild(agregaResultado)
+        return
+    }
+}
+
+function siguiente(){
+    amigoSorteado.innerText=""
+    botonSorteo.disabled = false
+}
+
+function soyYo(){
+    //Tome el ultimo valor agregado y lo guarde en una variable
+    persona = numerosSorteados.pop();
+    console.log(persona);
+    
+    //Generar otro número aleatorio y si es igual que el anterior hacer un bucle hasta que sea diferente 
+}
