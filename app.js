bd865ba501033da6ad397650e497d7309eb1efae
Eduardo Hernández <jhernandezg1403@gmail.com>
2025-02-27 10:15:37 -0600

    Añade función agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+let botonAñadir = document.querySelector('button')
+let nombreAmigo = document.getElementById('amigo')
+let listaAmigos = document.getElementById('listaAmigos')
+let valor = nombreAmigo.value
+console.log(valor)
+
+botonAñadir.addEventListener('click', agregarAmigo)
+
+function agregarAmigo() {
+    let valor = nombreAmigo.value
+    if (valor=="") {
+        alert('Ingresa un nombre válido')
+    } else {
+        console.log(valor)
+        nombres.push(valor)
+        nombreAmigo.value = "";
+        console.log(nombres)
+        
+    }
+}
+
+
+
